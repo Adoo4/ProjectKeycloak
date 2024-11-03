f886c0c960a89a625afd7f346580def540124864
User Name <email>
2024-11-03 16:10:13 +0100

    EventOr 1.1.5

--- src/Components/keycloak.jsx
+++ src/Components/keycloak.jsx
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'http://avajava.pro:8880',
+  url: 'https://avajava.pro:8880',
   realm: "praksa",
   clientId: "react-client",
 });
